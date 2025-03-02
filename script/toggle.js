94cac7994fbfbc8f0c07d0839f52786b8fcd30d1
nurulafsarinfo <mdnurulafsar123afsar@gmail.com>
2025-03-02 06:10:31 +0600

    added cashout featur

--- script/toggle.js
+++ script/toggle.js
@@ -1,6 +1,16 @@
+document.getElementById('cashout').style.display = 'none';
+
 document.getElementById('add-money-box').addEventListener('click',
     function(){
+        document.getElementById('add-money-section').style.display = 'block';
         document.getElementById('cashout').style.display = 'none';
+    }
+)
+
+document.getElementById('cashout-box').addEventListener('click',
+    function(){
+        document.getElementById('add-money-section').style.display = 'none';
         document.getElementById('cashout').style.display = 'block';
+
     }
 )
